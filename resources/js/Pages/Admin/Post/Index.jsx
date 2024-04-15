52e87220fe2050d96f7dc42928aca3946622f0c2
Zxigrid <ryuuhyakuy4@gmail.com>
2024-04-15 17:06:02 +0700

    chore: added key index

--- resources/js/Pages/Admin/Post/Index.jsx
+++ resources/js/Pages/Admin/Post/Index.jsx
@@ -61,6 +61,7 @@ export default function Index({ auth, posts }) {
           {
             posts.links && posts.links.map((link, _) => (
               <Link
+              key={_}
               as='Button'
               href={link.url}
               className="join-item btn btn-neutral"
