2a800a78e338b927d32397e3307daaafd1ae2d8f
Zxigrid <ryuuhyakuy4@gmail.com>
2024-04-16 02:24:53 +0700

    chore: format document's spaces

--- app/Http/Requests/Admin/Post/PostStoreRequest.php
+++ app/Http/Requests/Admin/Post/PostStoreRequest.php
@@ -6,38 +6,38 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class PostStoreRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'title' => ['required', 'string', 'max:255', 'min:5', 'unique:posts,title'],
-            'slug' => ['required', 'string', 'max:255', 'min:5', 'unique:posts,slug'],
-            'body' => ['required', 'string', 'min:5', 'max:255'],
-            'thumbnail' => ['image', 'required'],
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'title' => ['required', 'string', 'max:255', 'min:5', 'unique:posts,title'],
+      'slug' => ['required', 'string', 'max:255', 'min:5', 'unique:posts,slug'],
+      'body' => ['required', 'string', 'min:5', 'max:255'],
+      'thumbnail' => ['image', 'required'],
+    ];
+  }
 
-    public function data()
-    {
-      return [
-        'title' => $this->title,
-        'slug' => $this->slug,
-        'body' => $this->body,
-        'thumbnail' => $this->file('thumbnail'),
-        'user_id' => $this->user()->id,
-        'status' => 'draft'
-      ];
-    }
+  public function data()
+  {
+    return [
+      'title' => $this->title,
+      'slug' => $this->slug,
+      'body' => $this->body,
+      'thumbnail' => $this->file('thumbnail'),
+      'user_id' => $this->user()->id,
+      'status' => 'draft'
+    ];
+  }
 }
