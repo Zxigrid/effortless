321813e5d341bb1cd906dd3070922a8400972fc3
Zxigrid <ryuuhyakuy4@gmail.com>
2024-04-17 01:11:23 +0700

    chore: added new modal delete comp

---
+++ resources/js/Components/Tools/ModalDelete.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { AlertTriangle } from 'react-feather'
+import { router } from '@inertiajs/react'
+import Modal from '../Ui/Modal'
+
+const ModalDelete = ({link, data}) => {
+  const handleDelete = () => {
+    router.delete(link)
+  }
+  return (
+    <Modal
+      id={'modal_delete'}
+      Icon={AlertTriangle}
+      classNameIcon={'modal-icon error'}
+      title="Hapus Postingan"
+      description={`Apakah anda yakin ingin menghapus "${data}"?`}
+      cancel={
+        <button className='btn-action-sm default'>
+          Batalkan
+        </button>
+      }
+      approve={
+        <button onClick={handleDelete} className='btn-action-sm error'>
+          Hapus
+        </button>
+      }
+    />
+  )
+}
+
+export default ModalDelete
